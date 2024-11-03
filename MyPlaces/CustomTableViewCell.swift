6072a190a97475d2720a81e140bcb8f0308d7599
Graff095 <100046552+Graff095@users.noreply.github.com>
2024-11-03 20:55:36 +0300

    Custom Table View Cell

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Visarg on 03.11.2024.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageOfPlase: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var lacationLabel: UILabel!
+    @IBOutlet weak var tybeLabel: UILabel!
+}
