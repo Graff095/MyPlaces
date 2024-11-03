9f52da906c3f119e363dc3eee76c097c41086e2b
Graff095 <100046552+Graff095@users.noreply.github.com>
2024-11-03 19:45:22 +0300

    Initial commit

---
+++ MyPlacesTests/MyPlacesTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyPlacesTests.swift
+//  MyPlacesTests
+//
+//  Created by Visarg on 25.10.2024.
+//
+
+import Testing
+@testable import MyPlaces
+
+struct MyPlacesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
