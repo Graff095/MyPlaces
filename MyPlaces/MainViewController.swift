328c657cc344b03b2497ff58776aaf83bf7de5ea
Graff095 <100046552+Graff095@users.noreply.github.com>
2024-11-20 16:07:19 +0300

    Add place search functionality by name and location
    Добавлена функция поиска мест по имени и локации

--- MyPlaces/MainViewController.swift
+++ MyPlaces/MainViewController.swift
@@ -9,34 +9,59 @@ import UIKit
 import RealmSwift
 
 class MainViewController: UIViewController,UITableViewDelegate, UITableViewDataSource {
-
+    
+    private let searchController = UISearchController(searchResultsController: nil)
+    private var places: Results<Place>!
+    private var filteredPlaces: Results<Place>!
+    private var ascendingSorting = true
+    private var searchBarIsEmpty: Bool{
+        guard let text = searchController.searchBar.text else { return false}
+        return text.isEmpty
+    }
+    private var isFitering: Bool {
+        searchController.isActive && !searchBarIsEmpty
+    }
+    
     @IBOutlet weak var tableView: UITableView!
-    
     @IBOutlet weak var segmentedControl: UISegmentedControl!
-    
-    var places: Results<Place>!
-    var ascendingSorting = true
-    
-    
     @IBOutlet weak var reversedSortingButton: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         places = realm.objects(Place.self)
+        
+        // Setup the search controller - Настройка контроллера поиска
+        
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Поиск"
+        navigationItem.searchController = searchController
+        definesPresentationContext = true
+        
     }
 
+    
     // MARK: - Table view data source
 
-    
      func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+         
+         if isFitering{
+             return filteredPlaces.count
+         }
         return places.isEmpty ? 0 : places.count
     }
 
      func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CustomTableViewCell
         
-        let place = places[indexPath.row]
-        
+         var place = Place()
+         
+         if isFitering{
+             place = filteredPlaces[indexPath.row]
+         } else {
+             place = places[indexPath.row]
+         }
         cell.nameLabel.text = place.name
         cell.lacationLabel.text = place.location
         cell.tybeLabel.text = place.type
@@ -68,7 +93,13 @@ class MainViewController: UIViewController,UITableViewDelegate, UITableViewDataS
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "showDetall" {
             guard let indexPath = tableView.indexPathForSelectedRow else {return}
-            let place = places[indexPath.row]
+            var place: Place
+            if isFitering{
+                place = filteredPlaces[indexPath.row]
+            } else {
+                place = places[indexPath.row]
+            }
+            
             let newPlaceVC = segue.destination as! NewPlaceViewController
             newPlaceVC.currentPlace = place
           
@@ -112,6 +143,18 @@ class MainViewController: UIViewController,UITableViewDelegate, UITableViewDataS
             places = places.sorted(byKeyPath: "name", ascending: ascendingSorting)
         }
         
+        tableView.reloadData()
+    }
+}
+
+extension MainViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        filterContentForSearchText(searchController.searchBar.text!)
+    }
+    
+    
+    private func filterContentForSearchText(_ searchText: String) {
+        filteredPlaces = places.filter("name CONTAINS[c] %@ OR location CONTAINS[c] %@", searchText,searchText)
         tableView.reloadData()
     }
 }
