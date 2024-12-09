5d55533942fa69c82c7bd172f9f585f7eb97f165
Graff095 <100046552+Graff095@users.noreply.github.com>
2024-12-09 23:02:21 +0300

    Create MapViewController and implement UI
    Создан MapViewController и добавлен интерфейс

---
+++ MyPlaces/MapViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MapViewController.swift
+//  MyPlaces
+//
+//  Created by Visarg on 09.12.2024.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+     
+    }
+    
+
+    @IBAction func closeVC() {
+        
+        dismiss(animated: true)
+    }
+    
+
+}
