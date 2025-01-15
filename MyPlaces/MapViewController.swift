224caca731bf03ec62533255c1718e0a8d835cfd
Graff095 <100046552+Graff095@users.noreply.github.com>
2025-01-15 23:33:26 +0300

    Add route building functionality to a selected location
    Добавлена функция построения маршрута до выбранной локации

--- MyPlaces/MapViewController.swift
+++ MyPlaces/MapViewController.swift
@@ -21,12 +21,14 @@ class MapViewController: UIViewController {
     let locationManager = CLLocationManager()
     let regionInMeters = 10_000.00
     var incomeSegueIdentifier = ""
+    var placeCoordinate: CLLocationCoordinate2D?
     
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var mapPinImage: UIImageView!
     @IBOutlet weak var addressLabel: UILabel!
-    
     @IBOutlet weak var doneButton: UIButton!
+    @IBOutlet weak var goButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         addressLabel.text = ""
@@ -45,6 +47,10 @@ class MapViewController: UIViewController {
        
     }
     
+    @IBAction func goButtonPressed() {
+        getDirections()
+    }
+    
     @IBAction func closeVC() {
         
         dismiss(animated: true)
@@ -52,11 +58,15 @@ class MapViewController: UIViewController {
     
     
     private func setupMapView() {
+        
+        goButton.isHidden = true
+        
         if incomeSegueIdentifier == "showPlace" {
             setupPlacemark()
             mapPinImage.isHidden = true
             addressLabel.isHidden = true
             doneButton.isHidden = true
+            goButton.isHidden = false
         }
     }
     
@@ -77,6 +87,7 @@ class MapViewController: UIViewController {
             annotation.subtitle = self.place.type
             guard let placemarkLocation = placemark?.location else { return }
             annotation.coordinate = placemarkLocation.coordinate
+            self.placeCoordinate = placemarkLocation.coordinate
             self.mapView.showAnnotations([annotation], animated: true)
             self.mapView.selectAnnotation(annotation, animated: true)
             
@@ -137,6 +148,51 @@ class MapViewController: UIViewController {
     }
     
 
+    
+    private func getDirections () {
+        guard let location = locationManager.location?.coordinate else {
+            showAlert(title: "Error", message: "Текущее местоположение не найдено")
+            return }
+        
+        guard let request = createDirectionRequest(from: location) else {
+            showAlert(title: "Error", message: "Маршрут не найден")
+            return
+        }
+        
+        let directions = MKDirections (request: request)
+        directions.calculate { (response, error) in
+            
+            guard let response = response else {
+                self.showAlert(title: "Error", message: "Маршрут недоступен")
+                return
+            }
+            
+            for route in response.routes {
+                self.mapView.addOverlay(route.polyline)
+                self.mapView.setVisibleMapRect(route.polyline.boundingMapRect, animated: true)
+                let distance = String(format: "%.1f", route.distance / 1000)
+                let timeInterval = route.expectedTravelTime
+                
+                print("Расстояние до места: \(distance) км.")
+                print( "Время в пути составит: \(timeInterval) сек.")
+                
+            }
+        }
+    }
+    
+    private func createDirectionRequest(from coordinate: CLLocationCoordinate2D) -> MKDirections.Request? {
+        guard let destinationCoordinate = placeCoordinate else { return nil}
+        let startingLocation = MKPlacemark(coordinate: coordinate)
+        let destination = MKPlacemark(coordinate: destinationCoordinate)
+        let request = MKDirections.Request()
+        request.source = MKMapItem(placemark: startingLocation)
+        request.destination = MKMapItem(placemark: destination)
+        request.transportType = .automobile
+        request.requestsAlternateRoutes = true
+        return request
+        
+    }
+    
     private func getCenterLocation(for mapView: MKMapView) -> CLLocation {
         let latitude = mapView.centerCoordinate.latitude
         let longitude = mapView.centerCoordinate.longitude
@@ -203,6 +259,11 @@ extension MapViewController: MKMapViewDelegate{
         }
     }
   }
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+    let renderer = MKPolylineRenderer(overlay: overlay as! MKPolyline)
+    renderer.strokeColor = .blue
+    return renderer
+    }
 }
 
 extension MapViewController: CLLocationManagerDelegate {
